04e7ccf5e9b702de3843f5ab3148d15f2ef22102
kulakovskyi <torchok9@gmail.com>
2023-01-12 12:19:14 +0200

    add link and script

--- front/js/main.js
+++ front/js/main.js
@@ -140,3 +140,47 @@ function displayPopup(popup) {
     overlay.classList.remove('opacity-overlay')
     popup.classList.remove('hide')
 }
+
+
+
+(function () {
+    var url = new URL(window.location.href);
+    var params = ['l', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'param1', 'param2'];
+    var linkParams = ['affid', 'cpaid']; // ищем в url redirectUrl в url:
+
+    if (url.searchParams.has('redirectUrl')) {
+        var redirectUrl = new URL(url.searchParams.get('redirectUrl'));
+
+        if (redirectUrl.href.match(/\//g).length === 4 && redirectUrl.searchParams.get('l')) {
+            //если ссылка в ссылка redirectUrl корректная
+            localStorage.setItem('redirectUrl', redirectUrl.href); // указываем точкой входа домен с протоколом из redirectUrl
+        }
+    } /////////
+
+
+    params.forEach(function (param) {
+        if (url.searchParams.has(param)) localStorage.setItem(param, url.searchParams.get(param));
+    });
+    linkParams.forEach(function (linkParam) {
+        if (url.searchParams.has(linkParam)) localStorage.setItem(linkParam, url.searchParams.get(linkParam));
+    });
+    window.addEventListener('click', function (e) {
+        var link,
+            parent = e.target.closest('a');
+
+        if (parent.getAttribute('href') !== 'https://tds.favbet.partners') {
+            return;
+        }
+
+        parent && (e.preventDefault(),
+            localStorage.getItem("redirectUrl")
+                ? link = new URL(localStorage.getItem("redirectUrl"))
+                : (link = new URL(parent.href),
+                    affid = localStorage.getItem('affid'),
+                    cpaid = localStorage.getItem('cpaid'),
+                affid && cpaid && (link.pathname = '/' + affid + '/' + cpaid)), params.forEach(function (param)
+        {
+            url.searchParams.has(param) && link.searchParams.set(param, localStorage.getItem(param));
+        }), document.location.href = link);
+    });
+})();
